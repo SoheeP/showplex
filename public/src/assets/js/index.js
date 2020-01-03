f5e2b4a229c3bdb73f88ccb9776bdc94061ddf31
SoheeP_com <psh950502@naver.com>
2020-01-03 23:00:55 +0900

    add swiper slide and event section

--- public/src/assets/js/index.js
+++ public/src/assets/js/index.js
@@ -14,15 +14,83 @@ var mySwiper = new Swiper('.swiper-container-main', {
   speed: 500
 })
 
-var mySwiper = new Swiper('.swiper-container-part.movie', {
+var movieSlider = new Swiper('.swiper-container-part.movie', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-container-part.movie .section__slide__button-next',
-    prevEl: '.swiper-container-part.movie .section__slide__button-prev',
+    nextEl: '.swiper-container-part.movie .section__slide-part__button-next',
+    prevEl: '.swiper-container-part.movie .section__slide-part__button-prev',
+  },
+  speed: 500,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    720: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    }
+  }
+})
+
+var playSlider = new Swiper('.swiper-container-part.play', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-container-part.play .section__slide-part__button-next',
+    prevEl: '.swiper-container-part.play .section__slide-part__button-prev',
+  },
+  speed: 500,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    720: {
+      slidesPerView: 5,
+      spaceBetween: 60
+    }
+  }
+})
+
+var myswip = new Swiper('.swiper-container-part.musical', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-container-part.musical .section__slide-part__button-next',
+    prevEl: '.swiper-container-part.musical .section__slide-part__button-prev',
   },
   speed: 500,
   breakpoints: {
