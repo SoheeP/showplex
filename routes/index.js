7f3c1e3e0add833380dc615f64e32b0a70ef4d38
SoheeP_com <psh950502@naver.com>
2020-01-05 21:53:22 +0900

    add movie list, detail route html, and css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Index home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
 });
 
+// PAGE: Movie_list
+router.get('/movie/list', function(req, res, next){
+  res.render('pages/category/movie_list', { title : 'movie_list' })
+})
+
+// PAGE: Movie_detail
+router.get('/movie/detail', function(req, res, next){
+  res.render('pages/category/movie_detail', { title : 'movie_detail' })
+})
 module.exports = router;
