cda698dd5e469df5c0a22945580e0588d35a89f3
SoheeP_com <psh950502@naver.com>
2020-01-09 23:40:33 +0900

    add some link

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,15 @@ router.get('/movie/list', function(req, res, next){
 router.get('/movie/detail', function(req, res, next){
   res.render('pages/category/movie_detail', { title : 'movie_detail' })
 })
+
+// PAGE: Play_list
+router.get('/play/list', function(req, res, next){
+  res.render('pages/category/play_list', { title : 'play_list' })
+})
+
+// PAGE: Play_detail
+router.get('/play/detail', function(req, res, next){
+  res.render('pages/category/play_detail', { title : 'play_detail' })
+})
+
 module.exports = router;
