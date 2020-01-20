6c84307c6cf5ebb8020c313113c0aa9e706b929d
SoheeP <psh950502@naver.com>
2020-01-20 19:48:46 +0900

    add movie api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET Index home page. */
@@ -8,7 +9,26 @@ router.get('/', function(req, res, next) {
 
 // PAGE: Movie_list
 router.get('/movie/list', function(req, res, next){
-  res.render('pages/category/movie_list', { title : 'movie_list' })
+  let body = {};
+  let apiUrl = `https://yts.tl/api/v2`;
+  let recommendConfig = {
+    method: 'get',
+    url: `${apiUrl}/movie_suggestions.json?movie_id=10`
+  }
+  let ratedConfig = {
+    method: 'get',
+    url: `${apiUrl}/list_movies.json&limit=15`
+  };
+  axios(recommendConfig).then(function(res){
+    let recommendMovieData = res.data.data.movies;
+    body.recommendMovieData = recommendMovieData;
+  });
+  axios(ratedConfig).then(function(res){
+    let ratedMovieData = res.data.data.movies;
+    body.ratedMovieData = ratedMovieData;
+  });
+  body.title = 'movie_list'
+  res.render('pages/category/movie_list', body)
 })
 
 // PAGE: Movie_detail
@@ -19,7 +39,16 @@ router.get('/movie/detail', function(req, res, next){
 // PAGE: Play_list
 router.get('/play/list', function(req, res, next){
   res.render('pages/category/play_list', { title : 'play_list' })
+  // axios({
+    //   // config
+    //   method: 'get',
+    //   url: 'http://www.culture.go.kr/openapi/rest/publicperformancedisplays/d/MJd01k6JuHDKC6itg7A722SgdBKTKKnqXWo48ZZxWnNHPy4s6ODfypjmdUEwPRBqzzT6z5zUGrk1TSY2zdmjjw%3D%3D',
+    // }).then(function(res){
+    //   console.log(res)
+    // })
 })
+
+
 
 // PAGE: Play_detail
 router.get('/play/detail', function(req, res, next){
