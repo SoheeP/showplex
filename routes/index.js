aef441e9e9254de8508b91b1eeb5dd965ad2bc76
SoheeP <psh950502@naver.com>
2020-01-21 18:35:22 +0900

    fixed movie list, add movie detail

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var axios = require('axios');
+var { AxiosConfig } = require('./common');
 var router = express.Router();
 
 /* GET Index home page. */
@@ -8,32 +9,45 @@ router.get('/', function(req, res, next) {
 });
 
 // PAGE: Movie_list
-router.get('/movie/list', function(req, res, next){
+router.get('/movie/list', async function(req, res, next){
   let body = {};
   let apiUrl = `https://yts.tl/api/v2`;
-  let recommendConfig = {
-    method: 'get',
-    url: `${apiUrl}/movie_suggestions.json?movie_id=10`
-  }
   let ratedConfig = {
     method: 'get',
     url: `${apiUrl}/list_movies.json&limit=15`
   };
-  axios(recommendConfig).then(function(res){
-    let recommendMovieData = res.data.data.movies;
-    body.recommendMovieData = recommendMovieData;
-  });
-  axios(ratedConfig).then(function(res){
-    let ratedMovieData = res.data.data.movies;
-    body.ratedMovieData = ratedMovieData;
-  });
-  body.title = 'movie_list'
+  let recommendConfig = {
+    method: 'get',
+    url: `${apiUrl}/movie_suggestions.json?movie_id=10`
+  }
+  await axios.all([
+    AxiosConfig(ratedConfig), 
+    AxiosConfig(recommendConfig)
+  ])
+  .then(axios.spread(function(rated, recommend){
+    body.ratedMovieData = rated.data.data.movies;
+    body.recommendMovieData = recommend.data.data.movies;
+  }))
+  body.title = 'movie_list';
   res.render('pages/category/movie_list', body)
 })
 
 // PAGE: Movie_detail
-router.get('/movie/detail', function(req, res, next){
-  res.render('pages/category/movie_detail', { title : 'movie_detail' })
+router.get('/movie/detail/:seq', async function(req, res, next){
+  let body = {};
+  let movie_id = req.params.seq;
+  let movieDetailConfig = {
+    method: 'get',
+    url: `https://yts.tl/api/v2/movie_details.json?movie_id=${movie_id}`
+  }
+  await axios(movieDetailConfig).then(function(res){
+    body.movieInfo = res.data.data.movie;
+  }).catch(function(err){
+    console.log(`error: ${err}`)
+  })
+  body.title = 'movie_detail'
+  console.log(body, "DEEEEEEEEEEEEEETail");
+  res.render('pages/category/movie_detail', body)
 })
 
 // PAGE: Play_list
