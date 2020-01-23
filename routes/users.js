ab33cf5d9e93f6589085c7eb6edcde05c80b2805
SoheeP_com <psh950502@naver.com>
2020-01-23 21:56:42 +0900

    add users route path

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// PAGE: My page
 router.get('/mypage', function(req, res, next) {
   res.render('pages/mypage/dashboard')
+});
+
+// PAGE: Change one's profile
+router.get('/change_profile', function(req, res, next) {
+  res.render('pages/mypage/change_profile')
 });
 
 module.exports = router;
