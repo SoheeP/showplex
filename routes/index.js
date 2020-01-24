f399dbd36c7ce659d1a0a2950bdbd8488ad15da2
SoheeP <psh950502@naver.com>
2020-01-25 02:32:07 +0900

    add play list api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var axios = require('axios');
 var { AxiosConfig } = require('./common');
 var router = express.Router();
+var convert = require('xml-js');
+var request = require('request');
 
 /* GET Index home page. */
 router.get('/', function(req, res, next) {
@@ -40,26 +42,43 @@ router.get('/movie/detail/:seq', async function(req, res, next){
     method: 'get',
     url: `https://yts.tl/api/v2/movie_details.json?movie_id=${movie_id}`
   }
-  await axios(movieDetailConfig).then(function(res){
-    body.movieInfo = res.data.data.movie;
-  }).catch(function(err){
-    console.log(`error: ${err}`)
-  })
-  body.title = 'movie_detail'
-  console.log(body, "DEEEEEEEEEEEEEETail");
+  let sortArr = ['title', 'year', 'rating', 'peers', 'seeds', 'download_count', 'like_count', 'date_added'];
+  let randomSortby = sortArr[Math.floor(Math.random() * sortArr.length)];
+  let randomSortbyConfig = {
+    method: 'get',
+    url: `https://yts.tl/api/v2/list_movies.json?sort_by=${randomSortby}&limit=15`
+  }
+  await axios.all([
+    AxiosConfig(movieDetailConfig),
+    AxiosConfig(randomSortbyConfig)
+  ])
+  .then(axios.spread(function(detail, randomSort){
+    body.movieInfo = detail.data.data.movie;
+    body.randomSort = randomSort.data.data.movies;
+  }))
+  body.title = 'movie_detail';
+  // console.log(body, 'out body');
   res.render('pages/category/movie_detail', body)
 })
 
 // PAGE: Play_list
-router.get('/play/list', function(req, res, next){
-  res.render('pages/category/play_list', { title : 'play_list' })
-  // axios({
-    //   // config
-    //   method: 'get',
-    //   url: 'http://www.culture.go.kr/openapi/rest/publicperformancedisplays/d/MJd01k6JuHDKC6itg7A722SgdBKTKKnqXWo48ZZxWnNHPy4s6ODfypjmdUEwPRBqzzT6z5zUGrk1TSY2zdmjjw%3D%3D',
-    // }).then(function(res){
-    //   console.log(res)
-    // })
+router.get('/play/list', async function(req, res, next){
+  let body = {};
+  let HOST = `http://www.culture.go.kr/openapi/rest/publicperformancedisplays/realm`;
+  
+  // http://www.culture.go.kr/openapi/rest/publicperformancedisplays/period?from=20101118&to=20101217&cPage=1&rows=10&place=&gpsxfrom=&gpsyfrom=&gpsxto=&gpsyto=&keyword=&sortStdr=1&serviceKey=MJd01k6JuHDKC6itg7A722SgdBKTKKnqXWo48ZZxWnNHPy4s6ODfypjmdUEwPRBqzzT6z5zUGrk1TSY2zdmjjw%3D%3D
+  let KEY = `MJd01k6JuHDKC6itg7A722SgdBKTKKnqXWo48ZZxWnNHPy4s6ODfypjmdUEwPRBqzzT6z5zUGrk1TSY2zdmjjw%3D%3D`;
+  await axios.get(`${HOST}?requestTime=20200101&realmCode=A000&cPage=1&rows=10&place=&gpsxfrom=&gpsyfrom=&gpsxto=&gpsyto=&keyword=&sortStdr=1&${encodeURIComponent('serviceKey')}=${KEY}`
+  )
+  .then(function(res){
+    // console.log(res.data)
+    let convertJson = convert.xml2json(res.data, {compact: true, spaces: 2})
+    let resObj = JSON.parse(convertJson);
+    body.perforList = resObj.response.msgBody.perforList;
+  })
+  body.title = 'play_list';
+  console.log(body.perforList[0]);
+  res.render('pages/category/play_list', body)
 })
 
 
