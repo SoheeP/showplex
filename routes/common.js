11e79d0a6714afff8ac8f3ddb3312e0e4a25976f
soheeP <psh950502@naver.com>
2020-01-28 22:47:01 +0900

    divide module file

--- routes/common.js
+++ routes/common.js
@@ -1,7 +1,12 @@
-const axios = require('axios');
+const { axios, moment } = require('./npm_modules');
 
 function AxiosConfig(config){
   return axios(config);
 }
 
+function convertTimeToKorean(time){
+  return moment(time).format('YYYY년 MM월 DD일')
+}
+
 exports.AxiosConfig = AxiosConfig;
+exports.convertTimeToKorean = convertTimeToKorean;
---
+++ routes/npm_modules.js
@@ -0,0 +1,7 @@
+const xml_js = require('xml-js');
+const moment = require('moment');
+const axios = require('axios');
+
+exports.xml_js = xml_js;
+exports.moment = moment;
+exports.axios = axios;
