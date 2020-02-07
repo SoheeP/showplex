6909ae95c3d4c55e10332b9060f9197e22e7897f
soheeP <psh950502@naver.com>
2020-02-07 22:48:16 +0900

    add captcha module

--- routes/npm_modules.js
+++ routes/npm_modules.js
@@ -1,7 +1,11 @@
 const xml_js = require('xml-js');
 const moment = require('moment');
 const axios = require('axios');
+const svgCaptcha = require('svg-captcha');
+const uuid4 = require('uuid/v4');
 
 exports.xml_js = xml_js;
 exports.moment = moment;
 exports.axios = axios;
+exports.svgCaptcha = svgCaptcha;
+exports.uuid4 = uuid4;
