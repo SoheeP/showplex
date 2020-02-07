dfaade51b9522f606c6c3fc8d431f3c9003746e9
soheeP <psh950502@naver.com>
2020-02-07 22:49:21 +0900

    add captcha, uuid4 module

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {
+  Axios,
+  AxiosWithDB,
+  createSvgCaptcha
+} = require('./common');
+const { uuid4 } = require('./npm_modules');
 
-/* GET home page. */
-router.get('/signup', function(req, res, next) {
-  res.render('pages/auth/signup', { title: 'Sign up' });
-});
+// PAGE: Sign up
+router.route('/signup')
+.get(function(req, res, next) {
+  let captcha = createSvgCaptcha();
+  let body = {};
+  body.captcha = captcha;
+  req.session.captcha = captcha.text;
+  res.render('pages/auth/signup', body);
+}).post
+
+
 router.get('/signin', function(req, res, next) {
   res.render('pages/auth/signin', { title: 'Sign in' });
 });
