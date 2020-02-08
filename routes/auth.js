a3b6fc2df742bfa9a50ce31663ed99d6423f80cf
soheeP <psh950502@naver.com>
2020-02-08 23:26:55 +0900

    update signup function

--- routes/auth.js
+++ routes/auth.js
@@ -9,13 +9,38 @@ const { uuid4 } = require('./npm_modules');
 
 // PAGE: Sign up
 router.route('/signup')
-.get(function(req, res, next) {
+.get(async function(req, res, next) {
   let captcha = createSvgCaptcha();
   let body = {};
   body.captcha = captcha;
   req.session.captcha = captcha.text;
   res.render('pages/auth/signup', body);
-}).post
+})
+.post(function(req, res, next){
+  let email    = req.body.email,
+  password     = req.body.password,
+  username     = req.body.username,
+  phone        = req.body.phone,
+  verifyNumber = req.body.captcha;
+
+  if(req.session.captcha === verifyNumber){
+    const signupConfig = {
+      // DB router
+      url: `/auth/signup`,
+      method: 'post',
+      data: {
+        email: email,
+        password: password,
+        username: username,
+        phone: phone
+      }
+    }
+    AxiosWithDB(signupConfig, (response) => {
+      let { data } = response ;
+      console.log(data)
+    })
+  };
+});
 
 
 router.get('/signin', function(req, res, next) {
