5189816fc8e0fd140ad31e4e7efbf9615ea75f19
SoheeP <psh950502@naver.com>
2020-02-12 19:53:21 +0900

    dev - sign in

---
+++ public/src/assets/js/pages/auth/signin.js
@@ -0,0 +1,21 @@
+let signinForm = common.elm('.signin__form'),
+email          = common.elm('#email'),
+password       = common.elm('#password');
+
+signinForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const signinConfig = {
+    method: 'post',
+    url: '/auth/signin',
+    data: {
+      email: email.value,
+      password: password.value
+    }
+  };
+  axios(signinConfig).then((response) => {
+    let { data } = response;
+    if(data.result === 2){
+      alert('이메일이나 패스워드를 확인해주세요.')
+    }
+  })
+})
