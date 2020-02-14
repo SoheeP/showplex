605f39c36e062d8d9fe450dbbbb3d63a7f90fba8
SoheeP <psh950502@naver.com>
2020-02-14 20:33:58 +0900

    add isLogged function

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,12 +1,23 @@
-function wrap(asyncFn){
+function isLogged(req, res, next) {
+  console.log('Check out :: login');
+  if (!req.session.user) {
+    res.redirect('/auth/signin')
+  } else {
+    console.log('Login Suceess!')
+    next()
+  }
+}
+
+function wrap(asyncFn) {
   return (async (req, res, next) => {
-    try{
+    try {
       return await asyncFn(req, res, next);
-    } catch(error){
+    } catch (error) {
       console.log(error.message);
       return next(error);
     }
   })
 };
 
-exports.wrap = wrap;
+exports.isLogged = isLogged;
+exports.wrap     = wrap;
