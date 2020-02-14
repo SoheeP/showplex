8181a00e7199082377e417f59a1e9e99f76de912
SoheeP <psh950502@naver.com>
2020-02-14 20:36:57 +0900

    implement showing log and changing profile

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,64 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { isLogged, wrap } = require('./middlewares');
+const { AxiosConfig, AxiosWithDB } = require('./common');
 
 // PAGE: My page
-router.get('/mypage', function(req, res, next) {
-  res.render('pages/mypage/dashboard')
+router.route('/mypage')
+.get(isLogged, async (req, res, next) => {
+  let body = {};
+  let usernum = req.session.user.usernum;
+  let logConfig = {
+    url: `/users/mypage?user=${usernum}`
+  }
+  await AxiosWithDB(logConfig, (response) => {
+    let { data } = response;
+    body.logData = data.log;
+    console.log(body.logData);
+  })
+  res.render('pages/mypage/dashboard', body)
 });
 
 // PAGE: Change one's profile
-router.get('/change_profile', function(req, res, next) {
-  res.render('pages/mypage/change_profile')
+router.route('/change_profile').get(isLogged, (req, res, next) => {
+  /*
+  { usernum: 1,
+  email: 'abc@email.com',
+  username: 'P',
+  verify: null,
+  result: 1 }
+
+  */
+  let body = {};
+  body.userData = req.session.user;
+  console.log(req.session.user);
+  res.render('pages/mypage/change_profile', body)
 });
+
+router.route('/profile/change').post(isLogged, (req, res, next) => {
+  let usernum = req.session.user.usernum,
+  password = req.body.password,
+  username = req.body.username,
+  phone = req.body.phone;
+  console.log(password, username, phone, 'Want Change!!');
+
+  const changeConfig = {
+    url: `/users/profile/change`,
+    method: 'post',
+    data: {
+      usernum: usernum,
+      password: password,
+      username: username,
+      phone: phone,
+    }
+  }
+
+  AxiosWithDB(changeConfig, (response) => {
+    let { data } = response;
+    if(data.result === 1){
+      res.json({ result: 1 });
+    }
+  })
+})
 
 module.exports = router;
