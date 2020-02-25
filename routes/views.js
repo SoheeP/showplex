475cab5988187c0ca7f75abc16529cfb4e446dd9
SoheeP <psh950502@naver.com>
2020-02-26 00:22:32 +0900

    add faq, about route

--- routes/views.js
+++ routes/views.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
+// view - FAQ와 같은 정말로 보여지기만 하는 페이지
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/faq', function(req, res, next) {
+  res.render('pages/view_pages/faq', { title: 'Express' });
 });
+
+router.get('/about', (req, res, next) => {
+  res.render('pages/view_pages/about')
+})
 
 module.exports = router;
