1f95508b8e99210906ee609cb496e0b919b17508
SoheeP <psh950502@naver.com>
2020-03-16 21:56:54 +0900

    add a contact page

---
+++ public/src/assets/js/pages/view_pages/contact.js
@@ -0,0 +1,13 @@
+let copyBtn = common.elm('.contact__button-copy');
+
+copyBtn.addEventListener("click", (e)=>{
+  e.preventDefault();
+  let temp = document.createElement('textarea');
+  document.body.appendChild(temp);
+  temp.value = 'psh950502@naver.com';
+  // textarea 내용 전체 선택
+  temp.select();
+  document.execCommand('copy');
+  document.body.removeChild(temp);
+  alert('Copied!');
+})
